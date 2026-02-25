0d7672dc3774c0b9f67e150623624012a916b259
sandhya pandhare <sandhyapandhre22@gmail.com>
2026-02-25 11:54:39 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
-public class HelloWorld {
+public class DataTypesExample {
     public static void main(String[] args) {
-        System.out.println("Hello, cloudbatch sandhya!");
+        int number = 10;
+        double price = 19.99;
+        boolean isTrue = true;
+        // Variables hold different data types
+        System.out.println("Number: " + number + ", Price: " + price);
     }
-
 }
-
